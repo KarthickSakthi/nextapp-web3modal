8c52204fbead9b15962d87b20ee485a2452352c0
Karthick S <karthicksakthi124@gmail.com>
2023-03-05 17:38:58 +0000

    handle connect changes

--- nextapp-web3modal/src/pages/index.js
+++ nextapp-web3modal/src/pages/index.js
@@ -3,13 +3,40 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Web3Button } from '@web3modal/react'
+import { Web3Modal } from '@web3modal/react'
+import { ethers } from 'ethers'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import {
+
+  useConnect
+
+} from "wagmi";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { connectAsync } = useConnect();
+  const handleConnect = async() =>{
+    // console.log("handleConnect Triggered")
+    // const connector = new WalletConnectConnector({
+    //   options: {
+    //     qrcode: true,
+    //   },
+    // })
+    // const { account, chain } = await connectAsync({ connector });
+    // const web3modal = new Web3Modal();
+    const instance = await Web3Modal;
+    const provider =await new ethers.providers.Web3Provider(instance);
+    const signer = await provider.getSigner();
+    const address = await signer.getAddress();
+    console.log("address",address)
+  }
+
   return (
     <>
-     <Web3Button/>
+     {/* <Web3Button /> */}
+     <button onClick={handleConnect}>Connect Wallet</button>
     </>
   )
 }
